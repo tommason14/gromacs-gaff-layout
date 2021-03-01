40d78513c88a0023f287f97880742327d28dcd43
tommason14 <tom.mason14@gmail.com>
2021-03-01 15:51:25 +1100

    run

--- make_run.sh
+++ make_run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 module load gromacs/5.1.4
 # minimisation
-gmx_mpi grompp -f em/em.mdp -c pack/pack.pdb -p pack/polymer.top -o em/em.tpr
+gmx_mpi grompp -f em/em.mdp -c pack/pack.gro -p pack/polymer.top -o em/em.tpr
 # npt
-gmx_mpi grompp -f npt/npt.mdp -c em/em.gro -p pack/polymer.top -o npt/npt.tpr
+# gmx_mpi grompp -f npt/npt.mdp -c em/em.gro -p pack/polymer.top -o npt/npt.tpr
 # nvt
-gmx_mpi grompp -f nvt/nvt.mdp -c npt/npt.gro -p pack/polymer.top -o nvt/nvt.tpr
+# gmx_mpi grompp -f nvt/nvt.mdp -c npt/npt.gro -p pack/polymer.top -o nvt/nvt.tpr
--- pack/build.sh
+++
@@ -1,2 +0,0 @@
-make_gromacs_pdb.py 20 polymer.pdb -b 95 
-# script in github.com/tommason14/scripts
