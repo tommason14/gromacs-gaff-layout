a01dbbbb54175b49a267d0bc21b691e1b3ec0822
Tom Mason <tom.mason14@gmail.com>
2021-06-23 10:48:02 +1000

    Update make_run.sh

--- make_run.sh
+++ make_run.sh
@@ -4,5 +4,5 @@ module load gromacs/5.1.4
 gmx_mpi grompp -f em/em.mdp -c pack/pack.gro -p pack/polymer.top -o em/em.tpr
 # npt
 # gmx_mpi grompp -f npt/npt.mdp -c em/em.gro -p pack/polymer.top -o npt/npt.tpr
-# nvt
-# gmx_mpi grompp -f nvt/nvt.mdp -c npt/npt.gro -p pack/polymer.top -o nvt/nvt.tpr
+# nvt - using npt velocities
+# gmx_mpi grompp -f nvt/nvt.mdp -c npt/npt.gro -t npt/npt.cpt -p pack/polymer.top -o nvt/nvt.tpr
